c15debcae429d24d4aa3fbffa08bb199993abd98
Ferdinand <ndimbecha@gmail.com>
2021-04-01 13:23:58 +0200

    math pitfalls in JS done

---
+++ pitfalls.js
@@ -0,0 +1,8 @@
+let greetMe = "Hello";
+console.log(parseInt(greetMe));
+console.log(isNaN(3));
+//Rounding
+console.log((0.1 * 0.2).toFixed(2));
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
